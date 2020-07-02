c31e04e6b9d2a828d9dc5693df2dbbb7bbc87cb7
Kumar Kumbar <prateek.kumar.kumbar@accenture.com>
2020-07-02 16:41:17 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the Linear regression model
+filename = 'C:/Users/prateek.kumar.kumbar/Desktop/Portfolio/GRE/University-Admission-Predictor-Deployment/GRE_model2.pkl'
+model1 = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        gre = int(request.form['GRE'])
+        toefl = int(request.form['TOEFL'])
+        Univ = int(request.form['University'])
+        sop = int(request.form['SOP'])
+        lor = int(request.form['LOR'])
+        cgpa = float(request.form['CGPA'])
+        research = float(request.form['Research'])
+        #age = int(request.form['age'])
+        
+        data = np.array([[gre, toefl, Univ, sop, lor, cgpa, research]])
+        my_prediction = model1.predict(data)
+        my_prediction = my_prediction*100
+        my_prediction = my_prediction[0][0]
+        my_prediction = "{:.2f}".format(my_prediction)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
